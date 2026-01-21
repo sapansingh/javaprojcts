5dced5f177dd38e4db00920f005bdcca854dd16e
sapan singh <sapansinghchouhan@gmail.com>
2026-01-21 19:21:46 +0530

    .

--- ringlogic/src/main/java/com/fastagi/Callinfo.java
+++ ringlogic/src/main/java/com/fastagi/Callinfo.java
@@ -4,10 +4,9 @@ import org.asteriskjava.fastagi.BaseAgiScript;
 
 import com.fastagi.callinfo.callinfo;
 
-
 import org.asteriskjava.fastagi.AgiException;
 
-public class Test extends BaseAgiScript {
+public class Callinfo extends BaseAgiScript {
     @Override
     public void service(org.asteriskjava.fastagi.AgiRequest request,
                         org.asteriskjava.fastagi.AgiChannel channel) throws AgiException {
@@ -15,11 +14,11 @@ public class Test extends BaseAgiScript {
                  callinfo ci=new callinfo();
                  ci.getCallInfo();
 
-
-                            answer();
+                        answer();
                             System.out.println("Answered the call");
         streamFile("ConVox/namaskar"); // play welcome.wav from Asterisk sounds
         System.out.println("Played welcome message");
+        System.out.println("Inside Callinfo Class");
         setVariable("FASTAGI_OK", "1"); // optional, for your NoOp in dialplan
         
 
--- ringlogic/src/main/java/com/fastagi/config/Database.java
+++ ringlogic/src/main/java/com/fastagi/config/Database.java
@@ -1,10 +1,11 @@
 package com.fastagi.config;
 
-
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Database {
 
@@ -12,17 +13,41 @@ public class Database {
 
     static {
         try {
+            Properties props = new Properties();
+
+            try (InputStream is = Database.class
+                    .getClassLoader()
+                    .getResourceAsStream("db.properties")) {
+
+                if (is == null) {
+                    throw new RuntimeException("db.properties not found in classpath");
+                }
+                props.load(is);
+            }
+
             HikariConfig config = new HikariConfig();
-            config.setJdbcUrl("jdbc:mysql://192.168.29.72:3306/Synapse_cc");
-            config.setUsername("asterisk");
-            config.setPassword("asterisk");
-            config.setMaximumPoolSize(10);  // max concurrent connections
-            config.setMinimumIdle(2);       // minimum idle connections
-            config.setIdleTimeout(300000);  // 5 minutes
-            config.setConnectionTimeout(30000); // 30 seconds
-            config.setLeakDetectionThreshold(60000); // detect leaks > 60s
+            config.setJdbcUrl(props.getProperty("db.url"));
+            config.setUsername(props.getProperty("db.username"));
+            config.setPassword(props.getProperty("db.password"));
+
+            config.setMaximumPoolSize(
+                    Integer.parseInt(props.getProperty("db.pool.maxSize"))
+            );
+            config.setMinimumIdle(
+                    Integer.parseInt(props.getProperty("db.pool.minIdle"))
+            );
+            config.setIdleTimeout(
+                    Long.parseLong(props.getProperty("db.pool.idleTimeout"))
+            );
+            config.setConnectionTimeout(
+                    Long.parseLong(props.getProperty("db.pool.connectionTimeout"))
+            );
+            config.setLeakDetectionThreshold(
+                    Long.parseLong(props.getProperty("db.pool.leakDetection"))
+            );
 
             ds = new HikariDataSource(config);
+
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to initialize HikariCP pool", e);
